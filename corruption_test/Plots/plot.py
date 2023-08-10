afd89b4d4a0e8073016bf3261a93f5d270fab59a
kfoysalhaque <haque.k@northeastern.edu>
2023-08-10 16:02:56 -0400

    update

---
+++ corruption_test/Plots/plot.py
@@ -0,0 +1,67 @@
+# Imports
+import matplotlib.pyplot as plt
+import numpy as np
+from matplotlib import colors as mcolors
+import matplotlib as mpl
+
+mpl.rcParams['font.serif'] = 'Palatino'
+mpl.rcParams['text.usetex'] = 'true'
+mpl.rcParams['text.latex.preamble'] = r'\usepackage{newtxmath}'
+mpl.rcParams['font.size'] = 22
+#mpl.rcParams['axes.prop_cycle'] = plt.cycler(color=plt.cm.Set3.colors)
+
+
+# Change Params Here:
+file_string = 'object_detection_map50'
+rows_as_group = ['0', '10', '20', '30', '40', '50', '60', '70', '80', '90']
+columns_as_bars = ["YOLOv3", "YOLOv8"]
+ytick_label = ['0.0', '0.2', '0.4', '0.6', '0.7']
+
+
+data_file =  file_string + '.txt'
+fig_pdf_file = file_string + '.pdf'
+fig_eps_file = file_string + '.eps'
+
+
+data = np.loadtxt(data_file)
+data = data[:, 1:]
+
+column_1 = data[:, 0]
+column_2 = data[:, 1]
+
+
+x = np.arange(len(rows_as_group))
+width = 0.3
+fig, ax = plt.subplots()
+fig.set_figheight(5.5)
+fig.set_figwidth(12)
+
+rects1 = ax.bar(x - width, column_1, width, color='lavender', hatch='/', edgecolor="black", linewidth=2)
+rects2 = ax.bar(x, column_2, width, color='lightcyan', hatch=' \ ', edgecolor="black", linewidth=2)
+#rects3 = ax.bar(x + width, column_3, width, color='lavenderblush', hatch='//', edgecolor="black", linewidth=2)
+
+
+ax.set_ylabel('mAP$@$0.5', fontsize=35)
+ax.set_xlabel("Percent packet loss in each image", fontsize=35)
+#ax.set_title('Scores by group and gender', fontsize= 25)
+
+
+ax.set_xticks(x)
+ax.set_xticklabels(rows_as_group, fontsize=30)
+
+
+ax.set_ylim(0.0, 0.9)
+ax.set_yticklabels(ytick_label, fontsize=30)
+
+
+ax.legend([rects1, rects2], columns_as_bars, loc='upper center', ncol=2, fontsize=30)
+plt.grid(axis='y', linestyle='--', linewidth=0.5)
+plt.title('mAP$@$0.5 performance of object detection', fontsize=35)
+
+plt.tight_layout()
+
+plt.savefig(fig_pdf_file, dpi=300, format='pdf')
+plt.savefig(fig_eps_file, dpi=300, format='eps')
+
+plt.show()
+print('1')
---
+++ corruption_test/Plots/segment_PR/plot.py
@@ -0,0 +1,57 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# Read the CSV files
+files = ['yolov8_loss_0.csv', 'yolov8_loss_10.csv', 'yolov8_loss_20.csv', 'yolov8_loss_30.csv', 'yolov8_loss_40.csv', 'yolov8_loss_50.csv', 'yolov8_loss_60.csv', 'yolov8_loss_70.csv', 'yolov8_loss_80.csv', 'yolov8_loss_90.csv']
+fig_name = 'yolov8_segPR'
+
+
+fig_name_png = 'yolov8_segPR' + '.png'
+fig_name_pdf = 'yolov8_segPR' + '.pdf'
+
+xticks = [0.2, 0.4, 0.6, 0.8, 1.0]
+yticks = [0.0, 0.2, 0.4, 0.6, 0.8, 1.0]
+
+
+datasets = [pd.read_csv(file) for file in files]
+
+
+# Define padding values
+padding_values = [((0, 1), (1, 0))]
+
+# Apply padding to each dataset
+for data in datasets:
+    data.loc[0] = padding_values[0][0]
+    data.loc[-1] = padding_values[0][1]
+
+
+# Create a plot
+plt.figure(figsize=(13, 12))
+
+for idx, data in enumerate(datasets):
+    x = data.iloc[:, 0]
+    y = data.iloc[:, 1]
+    plt.plot(x, y, label=f'Packet {files[idx][7:-4]}%', linewidth=3)
+
+plt.xlim(0, 1)
+plt.ylim(0, 1)
+
+plt.xlabel('Recall', fontsize=45)
+plt.ylabel('Precision', fontsize=45)  # Increase legend font size
+
+plt.xticks(xticks, fontsize=35)
+plt.yticks(yticks, fontsize=35)
+
+plt.title('Precision-Recall plots with YOLOv8', fontsize=45)
+
+plt.grid(True)  # Add grid
+
+# Add legend with custom font size
+legend = plt.legend(fontsize=35)
+
+plt.tight_layout()
+plt.savefig(fig_name_png, dpi=300, format='png')
+plt.savefig(fig_name_pdf, dpi=300, format='pdf')
+
+plt.show()
+print('1')
